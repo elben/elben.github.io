e104a00dfd4e2c121527cb74bfef50ba3aea5728
Elben Shira <elbenshira@gmail.com>
2019-01-27 17:23:34 -1000

    Fix elbenshiracom.nix.
    Rebuild using:
    
    cabal2nix . > elbenshiracom.nix

--- elbenshiracom.nix
+++ elbenshiracom.nix
@@ -1,4 +1,4 @@
-{ mkDerivation, base, stdenv, text, unordered-containers }:
+{ mkDerivation, base, pencil, stdenv, text, unordered-containers }:
 mkDerivation {
   pname = "elbenshiracom";
   version = "0.1.0.0";
@@ -6,7 +6,7 @@ mkDerivation {
   isLibrary = false;
   isExecutable = true;
   executableHaskellDepends = [
-    base text unordered-containers
+    base pencil text unordered-containers
   ];
   homepage = "https://github.com/elben/elben.github.io";
   description = "Personal website: elbenshira.com";
